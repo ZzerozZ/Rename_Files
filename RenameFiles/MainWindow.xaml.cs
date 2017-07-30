f423c78b88819953a18f1e0bf4205805784bd44f
Dương Trọng Nghĩa <nghiaduong.170697@outlook.com.vn>
2017-07-30 09:08:20 +0700

    Add comment and fix display error

--- RenameFiles/MainWindow.xaml.cs
+++ RenameFiles/MainWindow.xaml.cs
@@ -18,32 +18,46 @@ namespace RenameFiles
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            bool result =  (MessageBoxResult.Yes == MessageBox.Show("Are you sure for rename all files on selected folder?\nYou can't go back when apply this request!", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes));
-            if (result)
+            bool isApply = (MessageBoxResult.Yes == MessageBox.Show("Are you sure for rename all files on selected folder?\nYou can't go back when apply this request!", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes));
+
+            if (isApply)
             {
                 //Do it!
                 if (txbFolder.Text != "")
                 {
                     string path = txbFolder.Text;
+                    //Get anyfile in this folder:
                     FileInfo[] file = (new DirectoryInfo(@path)).GetFiles();
 
+                    //Rename and change file format: 
                     foreach (FileInfo info in file)
                     {
                         info.MoveTo(@path + "//" + newName(info.Name));
+                        //If error existed, stop this event:
                         if (!done)
                             return;
                     }
 
+                    //Clear data in Window:
+                    txbNewFormat.Text = txbRemoveFrom.Text = txbRemoveTo.Text = txbReplace.Text = txbReplaceBy.Text = "";
+                   
+                    MessageBox.Show("Done!");
 
                 }
             }
         }
 
+        /// <summary>
+        /// Change the name of file
+        /// </summary>
+        /// <param name="oldName">Old name of file</param>
+        /// <returns>New name</returns>
         public string newName(string oldName)
         {
             done = true;
             string newname = "";
 
+            //Delete string from txbRemoveFrom to txbRemoveTo:
             try
             {
                 if (txbRemoveFrom.Text != "" && txbRemoveTo.Text != "")
@@ -57,13 +71,16 @@ namespace RenameFiles
                 done = false;
                 return oldName;
             }
-            
-                if (txbReplace.Text != "")
-                    newname = newname.Replace(txbReplace.Text, txbReplaceBy.Text);
-                else
-                    newname = oldName;
 
-            if(txbNewFormat.Text != "")
+
+            //Replace string txbReplace by txbReplaceBy:
+            if (txbReplace.Text != "")
+                newname = newname.Replace(txbReplace.Text, txbReplaceBy.Text);
+            else
+                newname = oldName;
+
+            //Change the file format:
+            if (txbNewFormat.Text != "")
             {
                 newname = newname.Remove(newname.LastIndexOf('.'), newname.Length - newname.LastIndexOf('.')) + txbNewFormat.Text;
             }
